fd241641b71987eb5c3dd1bdd4b071e951ce2e5c
shayanahmad1999 <shayanahmad235@gmail.com>
2025-09-02 17:31:32 +0500

    front side route are done

---
+++ routes/_portfolio.php
@@ -0,0 +1,15 @@
+
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{PageController, ProjectController, AjaxController};
+
+Route::get('/', [PageController::class, 'home'])->name('home');
+Route::get('/projects', [ProjectController::class, 'page'])->name('projects.page');
+Route::get('/contact', [PageController::class, 'contact'])->name('contact.page');
+
+Route::prefix('ajax')->group(function () {
+  Route::get('/projects', [AjaxController::class, 'projects'])->name('ajax.projects');
+  Route::get('/skills',   [AjaxController::class, 'skills'])->name('ajax.skills');
+  Route::post('/contact', [AjaxController::class, 'contact'])->name('ajax.contact');
+});
