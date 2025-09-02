732834a24851ff7e9764f5cc47394a533fb543a2
shayanahmad1999 <shayanahmad235@gmail.com>
2025-09-02 17:31:48 +0500

    define all route in mian route

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+// Portfolio routes
+require __DIR__ . '/_portfolio.php';
+
+// Admin routes
+require __DIR__ . '/admin.php';
+
+// Authentication routes
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
