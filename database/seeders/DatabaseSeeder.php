1e7385f60293c4d974aa1602aa663c672931f179
shayanahmad1999 <shayanahmad235@gmail.com>
2025-09-02 19:32:48 +0500

    refactor(database): update default user seeding with admin credentials
    Add password hashing for the default admin user and update email/name to reflect admin role

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\Hash;
+
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,8 +18,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password')
         ]);
     }
 }
