a3d2086509c6919fc93e1ccf9343e0b190ceb7bd
shayanahmad1999 <shayanahmad235@gmail.com>
2025-09-02 19:31:28 +0500

    SiteSettings and About model are created

---
+++ app/Models/About.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class About extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'content',
+        'image',
+        'resume_link',
+        'years_experience',
+        'completed_projects',
+        'companies_worked',
+    ];
+}
---
+++ app/Models/SiteSettings.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SiteSettings extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'site_title',
+        'site_description',
+        'primary_color',
+        'secondary_color',
+        'accent_color',
+        'button_color',
+        'button_hover_color',
+        'text_color',
+        'navbar_color',
+        'navbar_text_color',
+        'footer_color',
+        'footer_text_color',
+        'contact_email',
+        'contact_phone',
+        'contact_address',
+        'facebook_url',
+        'twitter_url',
+        'instagram_url',
+        'linkedin_url',
+        'github_url',
+    ];
+
+    /**
+     * Get a setting value by key
+     *
+     * @param string $key
+     * @param mixed $default
+     * @return mixed
+     */
+    public static function get($key, $default = null)
+    {
+        $setting = self::first();
+        
+        if (!$setting) {
+            return $default;
+        }
+        
+        return $setting->$key ?? $default;
+    }
+}
